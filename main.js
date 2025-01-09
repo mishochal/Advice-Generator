b2c88d844a6bb78dfd828fd552848ea4ce11fb53
Misho Chaladze <mishochaladze4@gmail.com>
2025-01-09 15:06:17 +0400

    add loading spinner

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const adviceId = document.querySelector(".advice-id");
 const adviceText = document.querySelector(".advice-text");
 const button = document.querySelector(".dice");
+const spinner = document.querySelector(".lds-dual-ring");
 
 const updateAdvice = (advice) => {
-    adviceId.innerText = "ADVICE #" + advice.id;
-    adviceText.innerText = advice.advice;
+    setTimeout(() => {
+        adviceId.innerText = "ADVICE #" + advice.id;
+        adviceText.innerText = advice.advice;
+        spinner.style.display = "none";
+    }, 250)
 }
 
 const getRandomAdvice = () => {
+    adviceText.innerText = "";
+    spinner.style.display = "inline-block";
     fetch("https://api.adviceslip.com/advice")
         .then(res => res.json())
         .then(advice => updateAdvice(advice.slip));
